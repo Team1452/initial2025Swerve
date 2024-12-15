67881f407e2da5594a2722157fe07ae43b87eb3b
whosawhatsis <whosawhatsis@gmail.com>
2024-12-14 16:35:42 -0800

    Another minor fix

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -36,7 +36,7 @@ import static frc.robot.subsystems.vision.VisionConstants.*;
 import frc.robot.subsystems.drive.DemoDrive;
 import frc.robot.subsystems.vision.Vision;
 import frc.robot.subsystems.vision.VisionIO;
-import frc.robot.subsystems.vision.VisionIOLimelight;
+import frc.robot.subsystems.vision.VisionIOPhotonVision;
 import frc.robot.subsystems.vision.VisionIOPhotonVisionSim;
 
 /**
@@ -49,7 +49,7 @@ public class RobotContainer {
   // Subsystems
   private final Drive drive;
   private final Vision vision;
-  //private final DemoDrive drive = new DemoDrive(); // Demo drive subsystem, sim only
+  private final DemoDrive demoDrive = new DemoDrive(); // Demo drive subsystem, sim only
 
   // Controller
   private final CommandXboxController controller = new CommandXboxController(0);
